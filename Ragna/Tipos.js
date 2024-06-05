baa59a3e90034f69d0807cba624a6b7c0aa59736
Ragnar <169070609+RagnarFerraz@users.noreply.github.com>
2024-06-05 12:43:16 -0300

    Aula de como aprender o que é objeto

---
+++ Ragna/Tipos.js
@@ -0,0 +1,9 @@
+let frutas = "Maça "
+let legumes = ["alho", "tomate", "abóbora"]
+let banana = {
+    "cor": "amarela",
+    "leguminosa": "Não",
+    "fruta": "Sim",
+    "sabor": "doce"
+}
+console.log("Vi na feira um cacho de bananas, a cor dela era " + banana.cor + " e o sabor é " + banana.sabor + ". Ela é uma fruta? " + banana.fruta + ". Ela é uma leguminosa? " + banana.leguminosa + ".")
