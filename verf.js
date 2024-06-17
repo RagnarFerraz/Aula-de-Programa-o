c7db548aacdcd04c8be4198f27441ae9ff96d7a3
Ragnar <169070609+RagnarFerraz@users.noreply.github.com>
2024-06-17 12:18:53 -0300

    aula sobre if

--- verf.js
+++ verf.js
@@ -1,8 +1,24 @@
-let result = 3 + 2
+let result = 3 + 3
+let carteira = {
+    "nacionalidade": "argentino",
+    "dinheiro": 15,
+    "idade": "16"
+}
+
 if(
-    result <= 5
+    carteira.nacionalidade == "argentino"
+) {
+    console.log(carteira.nacionalidade, "pode passar")
+}
+
+
+/*if(
+    result >= 5
 ) {
     console.log(result)
 }
 
-console.log("O código está certo")
+console.log("O código está certo")*/
+
+1
+0
