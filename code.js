274a726b5945759c87ebf0dbed824e1934ce1881
Ragnar <169070609+RagnarFerraz@users.noreply.github.com>
2024-07-17 12:17:20 -0300

    aula de interpretação

---
+++ code.js
@@ -0,0 +1,8 @@
+/*minha calça tem quatro bolsos, os dois primeiros sao os mais fundos, no primeiro tem minha carteira meu celular e minha chave, ja no segundo bolso uma outra chave, um cadeado e um cartão de visita*/
+let calça = {
+    "bolso1": ["celular", "chave", "carteira"],
+    "bolso2": ["chave", "cadeado", "cartao de visitas"],
+    "bolso3": [],
+    "bolso4": []
+}
+console.log(calça.bolso1 + calça.bolso2 + calça.bolso3 + " " + calça.bolso4)
